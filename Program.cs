2a0898e0767b8ed6ff91a9f642160c55a7ce1888
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-08-03 17:34:20 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Source source = new Source
+            {
+                Id = 1,
+                Name = "Ram",
+                Age = 21,
+                Occupation = "Engineer"
+
+            };
+
+            Destination destination = new Destination();
+
+            PropertyMapper.MapProperties(source, destination);
+
+            Console.WriteLine("Mapped properties in the Destination class:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Occupation: {destination.Occupation}");
+            Console.ReadKey();
+        }
+    }
+}
